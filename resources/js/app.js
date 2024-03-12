ad5e1899f5cecb14d2d5c674ce9a17ba731303ba
GBardeec <ilyas_muhamatdinov@mail.ru>
2024-03-12 14:19:38 +0400

    Installation and initial work with vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import { createApp } from 'vue';
+import Index from "@/components/Index.vue";
 import './bootstrap';
+import axios from 'axios';
+import router from './router';
+
+const app = createApp({
+    el: '#app',
+    components: {
+        Index,
+    }
+});
+
+if (localStorage.getItem('token')) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+}
+
+app.use(router);
+
+app.mount('#app');
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Отправка сообщений</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+</head>
+<body>
+    @yield('content')
+    <script src="https://kit.fontawesome.com/7c8801c017.js" crossorigin="anonymous"></script>
+</body>
+</html>
